c9a1d709c787d8e4080eb80e6d3191b03ee2677e
karthic <kar333thic@gmail.com>
2021-06-29 19:20:34 +0530

    added user inputs

--- 12thmarkcal.py
+++ 12thmarkcal.py
@@ -1,17 +1,17 @@
 
-mark_10_1 = 77
-mark_10_2 = 76
-mark_10_3 = 74
+mark_10_1 = int(input("Enter the 1st best mark you get in 10th : "))
+mark_10_2 = int(input("Enter the 2nd best mark you get in 10th : "))
+mark_10_3 = int(input("Enter the 3rd best mark you get in 10th : "))
 mark_10_total = mark_10_1+mark_10_2+mark_10_3
 mark_10_fulltotal = mark_10_total/300 * 50
 print("Total 10th mark %s" %int(mark_10_fulltotal))
 
-mark_11_tam = 49
-mark_11_eng = 42
-mark_11_mat = 25
-mark_11_phy = 15
-mark_11_che = 28
-mark_11_com = 48
+mark_11_tam = int(input("Enter the Tamil theory mark you get in 11th : "))
+mark_11_eng = int(input("Enter the English theory mark you get in 11th : "))
+mark_11_mat = int(input("Enter the Maths theory mark you get in 11th : "))
+mark_11_phy = int(input("Enter the Physics theory mark you get in 11th : "))
+mark_11_che = int(input("Enter the Chemistry theory mark you get in 11th : "))
+mark_11_com = int(input("Enter the ComputerScience theory mark you get in 11th : "))
 
 
 
